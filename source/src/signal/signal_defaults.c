d213e8b4f3300027125eed8cac82e066f3271082
Colin McGann <colinm22@iastate.edu>
2026-02-11 12:38:46 -0600

    Added signal default for SIG_CHILD

--- source/src/signal/signal_defaults.c
+++ source/src/signal/signal_defaults.c
@@ -4,4 +4,5 @@ signal_action_t signal_defaults[_SIG_COUNT] = {
     [SIG_ABORT] = ACT_TERMINATE,
     [SIG_ALARM] = ACT_TERMINATE,
     [SIG_PAGE]  = ACT_TERMINATE,
+    [SIG_CHILD] = ACT_IGNORE,
 };
