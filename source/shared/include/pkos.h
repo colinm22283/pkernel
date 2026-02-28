c2796907153ba65d195e2dfb79ad7ca29df255f4
Colin McGann <colinm22@iastate.edu>
2026-02-28 16:46:30 -0600

    Removed shared headers in favor of FSROOT

--- source/shared/include/pkos.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <pkos/defs.h>
-#include <pkos/syscalls.h>
-#include <pkos/types.h>
-#include <pkos/signal.h>
--- source/shared/include/pkos/defs.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <pkos/types.h>
-
-static const fd_t stdout = 0;
-static const fd_t stdin  = 1;
-
